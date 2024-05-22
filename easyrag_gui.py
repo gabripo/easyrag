c77d20f94fae1e5294c03a17231acf4ebc2d3785
gabripo <gabripo93+github@gmail.com>
2024-05-22 21:30:47 +0200

    Basic GUI to ask for data folder and used LLM

---
+++ easyrag_gui.py
@@ -0,0 +1,46 @@
+import PySimpleGUI as sg
+
+def ui_get_choices() -> list:
+    data_column = sg.Column([
+        [sg.Text("Select folder with documents:")],
+        [sg.In(size=(25,1), enable_events=True ,key='-FOLDER-'), sg.FolderBrowse()]
+    ])
+
+    supported_llms = ['llama3']
+    default_llm = 'llama3'
+    llm_column = sg.Column([
+        [sg.Text("Select LLM to use:")],
+        [sg.Combo(supported_llms, key='-LLM-', readonly=True, default_value=default_llm)],
+        [sg.Button("Submit")]
+    ])
+
+    layout = [
+        [data_column,
+        llm_column]
+    ]
+
+    window_title = 'Load folder'
+    window = sg.Window(window_title, layout)
+
+    choices_submitted = False
+    while True:
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED:
+            if not choices_submitted:
+                print("Window closed without submitting preferences!")
+                data_folder = None
+                llm = None
+            break
+        elif event == "Submit":
+            choices_submitted = True
+            data_folder = values['-FOLDER-']
+            llm = values['-LLM-']
+
+    window.close()
+    return [data_folder, llm]
+
+if __name__ == '__main__':
+    choices = ui_get_choices()
+    print('Chosen folder is: ', choices[0])
+    print('Chosen LLM is: ', choices[1])
