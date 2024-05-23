2bb6ca907c7381ed8a21a9ac556916d34a958923
gabripo <gabripo93+github@gmail.com>
2024-05-23 08:50:06 +0200

    Refactored variables and target folder

--- easyrag_gui.py
+++ easyrag_gui.py
@@ -3,7 +3,9 @@ import PySimpleGUI as sg
 def ui_get_options() -> dict:
     data_column = sg.Column([
         [sg.Text("Select folder with documents:")],
-        [sg.In(size=(25,1), enable_events=True ,key='-FOLDER-'), sg.FolderBrowse()]
+        [sg.In(size=(25,1), enable_events=True, key='-DATA_FOLDER-'), sg.FolderBrowse()],
+        [sg.Text("Select target folder for RAG data:")],
+        [sg.In(size=(25,1), enable_events=True, key='-RAG_FOLDER-'), sg.FolderBrowse()],
     ])
 
     supported_llms = ['llama3']
@@ -18,27 +20,25 @@ def ui_get_options() -> dict:
     ], element_justification='center', expand_x=True)
 
     layout = [
-        [data_column,
-        llm_column],
+        [data_column, llm_column],
         [options_column]
     ]
-
-    window_title = 'Load folder'
+    window_title = 'RAG options'
     window = sg.Window(window_title, layout)
 
     options_submitted = False
     user_options = {}
     while True:
         event, values = window.read()
-
         if event == sg.WIN_CLOSED:
             if not options_submitted:
                 print("Window closed without submitting options!")
             break
         elif event == "Submit":
             options_submitted = True
-            user_options['data_folder'] = values['-FOLDER-']
+            user_options['data_folder'] = values['-DATA_FOLDER-']
             user_options['llm'] = values['-LLM-']
+            user_options['rag_folder'] = values['-RAG_FOLDER-']
             break
 
     window.close()
@@ -46,8 +46,9 @@ def ui_get_options() -> dict:
 
 def ui_check_options(user_options={}) -> bool:
     if user_options and all(value for value in user_options.values()):
-        print('Chosen folder is: ', user_options['data_folder'])
-        print('Chosen LLM is: ', user_options['llm'])
+        print('Selected data folder is: ', user_options['data_folder'])
+        print('Selected target folder for RAG data is: ', user_options['rag_folder'])
+        print('Selected LLM is: ', user_options['llm'])
     else:
         print('Some user-specified options are missing!')
 
