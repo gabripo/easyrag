a73ccb365ad1a9e7a65b3bb82204dcdc0b1e20f5
gabripo <gabripo93+github@gmail.com>
2024-06-01 09:44:44 +0200

    Function to execute bash command and wait for execution

--- process_handler.py
+++ process_handler.py
@@ -21,3 +21,19 @@ def kill_process_by_pid(pid):
         print(f"Process with PID {pid} terminated successfully.")
     except ProcessLookupError:
         print(f"No process found with PID {pid}.")
+
+
+def execute_command_and_wait(command):
+    try:
+        process = subprocess.Popen(
+            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        process.communicate()  # Wait for the process to complete
+
+        if process.returncode == 0:
+            print(f"Command '{command}' executed successfully.")
+        else:
+            print(f"Error while executing command '{command}' .")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return
