65f6c9c31566dcbfc9d8264e43262eb560b64467
gabripo <gabripo93+github@gmail.com>
2024-11-09 15:51:13 +0100

    ollama embeddings come from related package
    from LangChain 1.0 the OllamaEmbeddings will not be part of langchain_community.embeddings

--- database_manager.py
+++ database_manager.py
@@ -3,19 +3,22 @@ import chromadb
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain_ollama import OllamaEmbeddings
 
-def build_database(docs_path='', model_name='llama3', db_folder_path='', persistent_db=True) -> Chroma:
+
+def build_database(
+    docs_path="", model_name="llama3", db_folder_path="", persistent_db=True
+) -> Chroma:
     if not docs_path:
         print("Unspecified documents path!")
         pass
-    if model_name == 'llama3':
+    if model_name == "llama3":
         embedding_model_function = OllamaEmbeddings(model=model_name)
     else:
         print("Unsupported model " + model_name)
         pass
     if not db_folder_path:
-        db_folder_path = os.path.join(docs_path, 'chroma_data')
+        db_folder_path = os.path.join(docs_path, "chroma_data")
 
     data = []
     for file in os.listdir(docs_path):
@@ -43,21 +46,22 @@ def build_database(docs_path='', model_name='llama3', db_folder_path='', persist
         vector_db.persist()
     return vector_db
 
-def load_database_from_folder(db_folder_path='', model_name='llama3') -> Chroma:
+
+def load_database_from_folder(db_folder_path="", model_name="llama3") -> Chroma:
     if not db_folder_path:
         print("Unspecified database path!")
         pass
     elif not os.path.exists(db_folder_path):
         print("Folder " + db_folder_path + "does not exist!")
         pass
-    if model_name == 'llama3':
+    if model_name == "llama3":
         embedding_model_function = OllamaEmbeddings(model=model_name)
     else:
         print("Unsupported model " + model_name)
         pass
 
     vector_db = Chroma(
-            persist_directory=db_folder_path,
-            embedding_function=embedding_model_function,
-        )
+        persist_directory=db_folder_path,
+        embedding_function=embedding_model_function,
+    )
     return vector_db
