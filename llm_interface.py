31bf38cbe4cb843ea5daf846793fe69fe13353ae
gabripo <gabripo93+github@gmail.com>
2024-11-13 19:25:57 +0100

    answer only if selected model is available

--- llm_interface.py
+++ llm_interface.py
@@ -3,6 +3,8 @@ import database_manager
 import chat_model
 from langchain.schema.runnable import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
+import requests
+import json
 
 
 def get_llm_response(query, user_options={}):
@@ -32,27 +34,45 @@ def get_llm_response(query, user_options={}):
 
 
 def get_llm_response_chat(query, user_options={}, chat_history=[]):
-    if os.path.exists(user_options["rag_folder"]):
-        print("Using already available database at path " + user_options["rag_folder"])
-        vector_db = database_manager.load_database_from_folder(
-            db_folder_path=user_options["rag_folder"], model_name=user_options["llm"]
+    llm_name = user_options["llm"]
+    if is_llm_available(llm_name):
+        if os.path.exists(user_options["rag_folder"]):
+            print(
+                "Using already available database at path " + user_options["rag_folder"]
+            )
+            vector_db = database_manager.load_database_from_folder(
+                db_folder_path=user_options["rag_folder"], model_name=llm_name
+            )
+        else:
+            vector_db = database_manager.build_database(
+                docs_path=user_options["data_folder"],
+                model_name=llm_name,
+                db_folder_path=user_options["rag_folder"],
+            )
+
+        num_tokens_to_consider = 3
+        retriever = vector_db.as_retriever(k=num_tokens_to_consider)
+
+        llm_model = chat_model.load_chat_model(model_name=llm_name)
+        contextualizer = chat_model.contextualize_system_prompt(llm_model, retriever)
+        qa_chain = chat_model.create_question_answer_chain(
+            llm_model, user_options["system_prompt"]
         )
+        chat_chain = chat_model.create_rag_chain(contextualizer, qa_chain)
+
+        answer = chat_chain.invoke({"input": query, "chat_history": chat_history})
+        return answer
     else:
-        vector_db = database_manager.build_database(
-            docs_path=user_options["data_folder"],
-            model_name=user_options["llm"],
-            db_folder_path=user_options["rag_folder"],
-        )
+        return {
+            "answer": f"Model {llm_name} is not available! Install it with Ollama before running Easyrag.\n"
+        }
 
-    num_tokens_to_consider = 3
-    retriever = vector_db.as_retriever(k=num_tokens_to_consider)
 
-    llm_model = chat_model.load_chat_model(model_name=user_options["llm"])
-    contextualizer = chat_model.contextualize_system_prompt(llm_model, retriever)
-    qa_chain = chat_model.create_question_answer_chain(
-        llm_model, user_options["system_prompt"]
-    )
-    chat_chain = chat_model.create_rag_chain(contextualizer, qa_chain)
-
-    answer = chat_chain.invoke({"input": query, "chat_history": chat_history})
-    return answer
+def is_llm_available(llm_name: str) -> bool:
+    api_url = "http://localhost:11434/api/generate"
+    headers = {"Content-Type": "application/json"}
+    data = {"model": llm_name, "prompt": "Hello!", "stream": False}
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
+    if response.status_code == 200:
+        return True
+    return False
