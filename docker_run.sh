4c1134c82303c90125459d29866a29d0a6a68b6a
gabripo <gabripo93+github@gmail.com>
2024-11-13 13:49:26 +0100

    script to run docker container with Flask

---
+++ docker_run.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if [ $# -eq 0 ]
+then
+    echo  "Default host port used: 3000"
+    HOST_PORT="3000"
+else
+    echo  "Specified host port will be used: $1"
+    HOST_PORT=$1
+fi
+
+echo "Link to the project homepage: http://0.0.0.0:$HOST_PORT"
+docker run -p $HOST_PORT:3000 docker-easyrag
